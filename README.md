364cea30f1c9cf5cf5eb406e6c22c77f0e81a534
unknown <br.delani@gmail.com>
2022-05-30 09:55:53 -0400

    Preparação do ambiente da API & conexão com o banco de dados estabelecida

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SimpleAPI
 API com Node, Nodemon e  Mongoose
+
+# Dependencias
+npm install express nodemon mongoose dotenv
+
+# Script
+Adicionado start": "nodemon ./index.js localhost 3000"
+ 
+ # Banco de Dados
+Estabelecendo conexão com o Cloud MongoDB
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const server = express()
+
+// ler JSON / middlewares
+server.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+
+server.use(express.json())
+
+// rota inicial
+server.get('/', (req, res) => {
+
+    res.json({message: "Rota home funcionando!"})
+})
+
+// estabelecendo conexão com o bando de dados
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.w1es0.mongodb.net/?retryWrites=true&w=majority`)
+.then(() => {
+    console.log("Estabeleceu conexão com o bando de dados!")
+    // setar uma porta
+    server.listen(3000)
+})
+.catch((err) => console.log(err))
